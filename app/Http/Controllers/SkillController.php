a50318daef93fed600184d5b6cea6aacca202882
bellsilver7 <bellsilver7@gmail.com>
2022-11-19 06:55:00 +0900

    store new skill

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Skill;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -15,5 +16,25 @@ class SkillController extends Controller
     public function create()
     {
         return Inertia::render('Skills/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image' => ['required', 'image'],
+            'name' => ['required', 'min:3'],
+        ]);
+
+        if ($request->has('image')) {
+            $image = $request->file('image')->store('skills');
+            Skill::create([
+                'name' => $request->name,
+                'image' => $image,
+            ]);
+
+            return redirect()->route('skills.index');
+        }
+
+        return redirect()->back();
     }
 }
