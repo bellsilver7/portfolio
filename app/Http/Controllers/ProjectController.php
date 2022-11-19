6c0fc5f6cbce430d8ec92820fce0806dc77b8604
bellsilver7 <bellsilver7@gmail.com>
2022-11-20 07:14:00 +0900

    store new project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -14,6 +16,30 @@ class ProjectController extends Controller
 
     public function create()
     {
-        return Inertia::render('Projects/Create');
+        $skills = Skill::all();
+        return Inertia::render('Projects/Create', compact('skills'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image' => ['required', 'image'],
+            'name' => ['required', 'min:3'],
+            'skill_id' => ['required', 'min:3'],
+        ]);
+
+        if ($request->has('image')) {
+            $image = $request->file('image')->store('projects');
+            Project::create([
+                'skill_id' => $request->skill_id,
+                'name' => $request->name,
+                'image' => $image,
+                'project_url' => $request->project_url,
+            ]);
+
+            return redirect()->route('projects.index');
+        }
+
+        return redirect()->back();
     }
 }
