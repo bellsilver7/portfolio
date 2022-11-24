db424e22fb484c4d8116a4209615a738e7aea16e
bellsilver7 <bellsilver7@gmail.com>
2022-11-24 16:50:25 +0900

    Create Hero

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { Head, Link } from "@inertiajs/inertia-vue3";
 import FrontendLayout from "@/Layouts/FrontendLayout.vue";
+import Hero from "@/Layouts/Frontend/Hero.vue";
 </script>
 
 <template>
     <Head title="Welcome to portfolio" />
     <FrontendLayout>
-        <!-- Hero primary -->
+        <Hero />
         <!-- Promote tail-100 -->
         <!-- About secondary -->
         <!-- Skills tail-100 -->
